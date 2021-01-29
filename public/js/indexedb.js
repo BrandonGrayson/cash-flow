eb8cb62a45d57f02211af1d6abdd5a0818ed6054
Brandon <brandonbgrayson@gmail.com>
2021-01-28 20:44:35 -0500

    created temporary objectStore called data

--- public/js/indexedb.js
+++ public/js/indexedb.js
@@ -1,6 +1,14 @@
 // request a database instance
 const request = indexedDB.open("budget", 1);
 
+
+// create an object store inside the onupgradeneeded method
+request.onupgradeneeded = ({target}) => {
+    const db = target.result;
+    const objectStore = db.createObjectStore("Data")
+};
+
+// on success log the event to the console
 request.onsuccess = event => {
     console.log(request.result.name)
-}
+};
