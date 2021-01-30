fc2501ac3a9f7d9d78bf98aa46ab1dc0eda15d0d
Brandon <brandonbgrayson@gmail.com>
2021-01-30 17:13:15 -0500

    added service worker var and files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
-console.log("")
+// cache variables
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png',
+  ];
