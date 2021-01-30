6ee33e2c8731712beab80efab1e50626080d6d61
Brandon <brandonbgrayson@gmail.com>
2021-01-30 18:19:24 -0500

    added activate event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,15 +9,34 @@ const FILES_TO_CACHE = [
     '/manifest.webmanifest',
     '/assets/images/icons/icon-192x192.png',
     '/assets/images/icons/icon-512x512.png',
-  ];
+];
 
   // install
-  self.addEventListener("install", function (event) {
-      event.waitUntil(
-          caches.open(DATA_CACHE_NAME).then((cache) => {
-              console.log("Files were pre-cached successfully");
-              return cache.addAll(FILES_TO_CACHE);
-          })
-      );
-      self.skipWaiting()
-  });
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+            console.log("Files were pre-cached successfully");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting()
+});
+
+// activate
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key)
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+
