878a02e44410fc07132e7ef0fa039dbafc854313
Brandon <brandonbgrayson@gmail.com>
2021-01-30 12:23:10 -0500

    set up transaction keypath

--- public/js/indexedb.js
+++ public/js/indexedb.js
@@ -1,15 +1,19 @@
 // request a database instance
 const request = indexedDB.open("budget", 1);
 
-
 // create an object store inside the onupgradeneeded method
 request.onupgradeneeded = ({target}) => {
     const db = target.result;
-    const objectStore = db.createObjectStore("Data");
-    objectStore.createIndex("timestamp", "timestamp")
+    const transaction = db.createObjectStore("transaction", {keypath: "transaction"})
+
 };
 
 // on success log the event to the console
 request.onsuccess = event => {
     console.log(request.result.name)
+
+    const objectStore = db.createObjectStore("transaction");
+    objectStore.createIndex("name", "name");
+    objectStore.createIndex("value", "value");
+    objectStore.createIndex("date", "date");
 };
