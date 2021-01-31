3b1a8cc0460a6738c825fdcbc734eb12731c7cd4
Brandon <brandonbgrayson@gmail.com>
2021-01-31 12:48:44 -0500

    service worker scope is returning err

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,11 +5,12 @@ const DATA_CACHE_NAME = "data-cache-v1";
 // files to cache
 const FILES_TO_CACHE = [
     '/',
+    '/api/transaction',
     '/index.html',
-    '../routes/api.js',
+    '/routes/api.js',
     '/manifest.webmanifest',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-512x512.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
 ];
 
   // install
